b83b0236b46ec42867c75454d5f9f042958fb2eb
Nick LaMuro <nicklamuro@gmail.com>
2014-04-16 17:07:15 -0500

    Updates queue shared example
    - Removes 'should' from descriptions
    - Moves a nests a describe block to a new location
    - Uses "context" where appropriate

--- spec/support/queue_shared_example.rb
+++ spec/support/queue_shared_example.rb
@@ -12,7 +12,7 @@ shared_examples "a queue" do
   it { should respond_to :size }
 
   describe "pop" do
-    describe "with messages" do
+    context "with messages" do
       before do
         not_for_null do
           client.stub queue: queue_object_with_message
@@ -20,35 +20,35 @@ shared_examples "a queue" do
         end
       end
 
-      it "should simply return a message" do
+      it "returns a message" do
         not_for_null do
           subject.pop.should be_a Queuel::Base::Message
         end
       end
 
-      it "should delete after block" do
+      it "deletes the message after block" do
         not_for_null do
           message.should_receive(:delete)
           subject.pop { |m| m }
         end
       end
     end
-  end
 
-  describe "with nil message" do
-    before do
-      not_for_null do
-        client.stub queue: queue_object_with_nil_message
-        client.stub_chain :queues, named: queue_object_with_nil_message
+    context "with nil message" do
+      before do
+        not_for_null do
+          client.stub queue: queue_object_with_nil_message
+          client.stub_chain :queues, named: queue_object_with_nil_message
+        end
       end
-    end
 
-    it "should simply return a message" do
-      subject.pop.should == nil
-    end
+      it "returns nil" do
+        subject.pop.should == nil
+      end
 
-    it "should delete after bolck" do
-      subject.pop { |m| m } # basically, don't error
+      it "does not error" do
+        subject.pop { |m| m } # basically, don't error
+      end
     end
   end
 end
