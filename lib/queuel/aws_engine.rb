9030e498eb9db2834b6d4a15d6a17ebc0cee2f36
Nick Sieger <nick@nicksieger.com>
2017-08-31 22:27:44 -0500

    Check string and symbol keys here

--- lib/queuel/aws_engine.rb
+++ lib/queuel/aws_engine.rb
@@ -2,7 +2,7 @@ module Queuel
   module AwsEngine
     def client
       @client ||= client_klass.new(
-        region: credentials[:region] || 'us-east-1',
+        region: credentials['region'] || credentials[:region] || 'us-east-1',
         credentials: Aws::Credentials.new(
           credentials['access_key_id'] || credentials[:access_key_id],
           credentials['secret_access_key'] || credentials[:secret_access_key]
